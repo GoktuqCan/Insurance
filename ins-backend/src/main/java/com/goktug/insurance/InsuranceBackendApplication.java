40f81985d77c045314a740b8a8ea456538aaabf2
goktug.cakmak <goktug.cakmak@obss.com.tr>
2018-07-24 14:40:41 +0300

    Backend Initial Commit
    Backend Initial Commit

---
+++ ins-backend/src/main/java/com/goktug/insurance/InsuranceBackendApplication.java
@@ -0,0 +1,12 @@
+package com.goktug.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceBackendApplication.class, args);
+	}
+}
---
+++ ins-backend/src/test/java/com/goktug/insurance/InsuranceBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goktug.insurance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsuranceBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
